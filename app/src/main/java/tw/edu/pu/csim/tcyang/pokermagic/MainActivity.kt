95f35755eca336c258a7e108be3d696737b45d5c
tcyang1971 <tcyang@gm.pu.edu.tw>
2022-04-01 11:23:03 +0800

    v.2 圖片觸控監聽

--- app/src/main/java/tw/edu/pu/csim/tcyang/pokermagic/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/pokermagic/MainActivity.kt
@@ -2,11 +2,24 @@ package tw.edu.pu.csim.tcyang.pokermagic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnTouchListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        img.setOnTouchListener(this)
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        if(event?.action == MotionEvent.ACTION_DOWN){
+            txv.text = "手指按下"
+        }
+        if(event?.action == MotionEvent.ACTION_UP){
+            txv.text = "手指彈開"
+        }
+        return true
     }
 }
